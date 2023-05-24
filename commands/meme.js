7790cb17a2d598555e24e95ee4643a93c5dea36d
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-05-24 13:45:31 +0000

    feat: Add 'pip' command
    - This commit adds the 'pip' command, which allows users to retrieve information about a pip package.

--- commands/meme.js
+++ commands/meme.js
@@ -2,28 +2,48 @@ const axios = require("axios");
 const { InlineKeyboard } = require("grammy");
 
 module.exports = {
-  name: "meme",
-  description: "Sends a random meme from Reddit",
-  category: "Fun",
+  name: "pip",
+  description: "Get info about a pip package",
+  category: "Utilities",
   handler: async (ctx) => {
+    const { message } = ctx;
+    const { text } = message;
+
+    if (!text.includes(" ")) {
+      await ctx.reply("Please provide a package name");
+      return;
+    }
+
+    const packageName = text.substring(5);
     try {
-      await ctx.api.sendChatAction(ctx.chat.id, "typing");
+      const response = await axios.get(
+        `https://pypi.org/pypi/${packageName}/json`
+      );
+      const packageInfo = response.data.info;
 
-      const response = await axios.get("https://meme-api.com/gimme");
-      const { title, postLink, subreddit, url } = response.data;
-
-      const inlineKeyboard = new InlineKeyboard().url(
-        "View on Reddit",
-        postLink
+      const buttons = new InlineKeyboard().url(
+        "View on PyPI",
+        `https://pypi.org/project/${packageName}`
       );
 
-      await ctx.replyWithPhoto(url, {
-        caption: `${title}\n\nPosted on r/${subreddit}`,
-        reply_markup: inlineKeyboard,
-      });
+      await ctx.reply(
+        `<b>Package name:</b> <code>${packageInfo.name}</code>\n<b>Version:</b> <code>${packageInfo.version}</code>\n<b>Author:</b> <code>${packageInfo.author}</code>\n<b>Author email:</b> <code>${packageInfo.author_email}</code>\n<b>Home page:</b> ${packageInfo.home_page}\n<b>License:</b> <code>${packageInfo.license}</code>\n<b>Summary:</b> <code>${packageInfo.summary}</code>`,
+        {
+          reply_markup: buttons,
+          parse_mode: "HTML",
+        }
+      );
     } catch (error) {
-      console.error("Error fetching meme:", error);
-      await ctx.reply("Oops! An error occurred while fetching the meme.");
+      if (error.response && error.response.status === 404) {
+        await ctx.reply(`Package <code>${packageName}</code> not found`, {
+          parse_mode: "HTML",
+        });
+      } else {
+        await ctx.reply(
+          `An error occurred while fetching package <code>${packageName}</code> information`,
+          { parse_mode: "HTML" }
+        );
+      }
     }
   },
 };
