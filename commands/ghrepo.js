62fb593d773a7b1fd34807724d2c5df13c197c43
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-05-24 09:39:22 +0000

    feat: Add GitHub repository info command
    - This commit adds a new command, 'ghrepo', to retrieve information about a GitHub repository. The command allows users to provide the name of a repository and fetches details such as the repository name, description, language, stars, forks, open issues, license, and owner. The retrieved information is presented in a formatted reply message with a link to view the repository on GitHub.

---
+++ commands/ghrepo.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const { InlineKeyboard } = require("grammy");
+
+module.exports = {
+  name: "ghrepo",
+  description: "Get info about a GitHub repository",
+  category: "Git Utilities",
+  handler: async (ctx) => {
+    const { message } = ctx;
+    const { text } = message;
+
+    if (!text.includes(" ")) {
+      await ctx.reply("Please provide a name for the repository");
+      return;
+    }
+
+    const repo = text.substring(text.indexOf(" ") + 1);
+
+    try {
+      const response = await axios(
+        `https://api.github.com/search/repositories?q=${repo}`
+      );
+
+      const data = response.data.items[0];
+
+      await ctx.reply(
+        `<b>Repository:</b> <code>${data.name}</code>\n<b>Description:</b> ${data.description}\n<b>Language:</b> ${data.language}\n<b>Stars:</b> ${data.stargazers_count}\n<b>Forks:</b> ${data.forks_count}\n<b>Open Issues:</b> ${data.open_issues_count}\n<b>License:</b> ${data.license.name}\n<b>Owner:</b> ${data.owner.login}`,
+        {
+          reply_markup: new InlineKeyboard().url(
+            "View on GitHub",
+            data.html_url
+          ),
+          disable_web_page_preview: true,
+          parse_mode: "HTML",
+        }
+      );
+    } catch (error) {
+      await ctx.reply(
+        "Oops, an error occurred!\n\nPossible reasons:\n• User not found\n• Rate limit exceeded"
+      );
+    }
+  },
+};
