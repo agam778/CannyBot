b0d646ecbf91d07e158bfdb2b66b9a1172f73b80
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-05-24 18:53:52 +0000

    feat: Add "meme" command
    - Introduce a new command called "meme" that sends a random meme from Reddit.

---
+++ commands/meme.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+const { InlineKeyboard } = require("grammy");
+
+module.exports = {
+  name: "meme",
+  description: "Sends a random meme from Reddit",
+  category: "Fun",
+  handler: async (ctx) => {
+    try {
+      await ctx.api.sendChatAction(ctx.chat.id, "typing");
+
+      const response = await axios.get("https://meme-api.com/gimme");
+      const { title, postLink, subreddit, url } = response.data;
+
+      const inlineKeyboard = new InlineKeyboard().url(
+        "View on Reddit",
+        postLink
+      );
+
+      await ctx.replyWithPhoto(url, {
+        caption: `${title}\n\nPosted on r/${subreddit}`,
+        reply_markup: inlineKeyboard,
+      });
+    } catch (error) {
+      console.error("Error fetching meme:", error);
+      await ctx.reply("Oops! An error occurred while fetching the meme.");
+    }
+  },
+};
