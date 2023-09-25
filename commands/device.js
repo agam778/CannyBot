23e76c3291f693a515fee07d9af37a987efbd760
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-09-25 19:59:12 +0530

    feat: Add device command

---
+++ commands/device.js
@@ -0,0 +1,55 @@
+const { InlineKeyboard } = require('grammy')
+const axios = require('axios')
+
+module.exports = {
+  name: 'device',
+  description: 'Get codename and name of given Android device',
+  usage: '/device <codename or name>',
+  example: '/device RMX3370',
+  category: 'Utilities',
+  handler: async (ctx) => {
+    const { message } = ctx
+    const { text } = message
+
+    if (!text.includes(' ')) {
+      await ctx.reply('Please provide a device codename or name')
+      return
+    }
+
+    const deviceQuery = text.substring(text.indexOf(' ') + 1)
+    const url = `https://raw.githubusercontent.com/ejbtrd/android_selected_certified_devices/main/devices.json`
+
+    try {
+      const data = await axios.get(url)
+      const responseData = data.data
+
+      if (!responseData) {
+        await ctx.reply('Oops, something went wrong')
+        return
+      }
+
+      const result = responseData.filter(
+        (device) =>
+          device.name.toLowerCase().includes(deviceQuery.toLowerCase()) ||
+          device.codename.toLowerCase().includes(deviceQuery.toLowerCase()) ||
+          device.model.toLowerCase().includes(deviceQuery.toLowerCase())
+      )
+      if (result.length === 0) {
+        await ctx.reply('No device found')
+        return
+      }
+
+      let reply = 'Devices found for query `' + deviceQuery + '`:\n\n'
+
+      result.forEach((foundDevice) => {
+        reply += `${foundDevice.name}: \`${foundDevice.codename}\`\n`
+      })
+
+      await ctx.reply(reply, {
+        parse_mode: 'Markdown',
+      })
+    } catch (error) {
+      await ctx.reply('Oops, something went wrong\n\n' + ```${error}```)
+    }
+  },
+}
