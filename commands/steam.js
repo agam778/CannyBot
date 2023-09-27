716595b4b2fd781608413b6b81fa1d8bffce50b1
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-09-27 23:34:34 +0530

    CannyBot: steam: remove try-catch block

--- commands/steam.js
+++ commands/steam.js
@@ -20,82 +20,82 @@ module.exports = {
     const game = text.substring(text.indexOf(' ') + 1)
     const url = `https://api.steampowered.com/ISteamApps/GetAppList/v2/`
 
-    try {
-      const response = await axios.get(url)
-
-      if (
-        !response ||
-        !response.data ||
-        !response.data.applist ||
-        !response.data.applist.apps
-      ) {
-        await ctx.reply('Failed to fetch game data from Steam.')
+    const response = await axios.get(url).catch((err) => {
+      if (err.response.status === 404) {
+        ctx.reply('Failed to fetch game data from Steam.')
         return
       }
+    })
 
-      const games = response.data.applist.apps
+    if (
+      !response ||
+      !response.data ||
+      !response.data.applist ||
+      !response.data.applist.apps
+    ) {
+      await ctx.reply('Failed to fetch game data from Steam.')
+      return
+    }
 
-      const matchingGames = games.filter((gameObj) =>
-        gameObj.name.toLowerCase().includes(game.toLowerCase())
-      )
+    const games = response.data.applist.apps
 
-      if (matchingGames.length === 0) {
-        await ctx.reply('Game not found on Steam.')
-        return
-      }
+    const matchingGames = games.filter((gameObj) =>
+      gameObj.name.toLowerCase().includes(game.toLowerCase())
+    )
 
-      const matchingGame = matchingGames[0]
-      const gameName = matchingGame.name
-      const gameAppID = matchingGame.appid
+    if (matchingGames.length === 0) {
+      await ctx.reply('Game not found on Steam.')
+      return
+    }
 
-      const gameDetailsUrl = `https://store.steampowered.com/api/appdetails/?appids=${gameAppID}`
-      const gameDetailsResponse = await axios.get(gameDetailsUrl)
+    const matchingGame = matchingGames[0]
+    const gameName = matchingGame.name
+    const gameAppID = matchingGame.appid
 
-      if (
-        !gameDetailsResponse ||
-        !gameDetailsResponse.data ||
-        !gameDetailsResponse.data[gameAppID] ||
-        !gameDetailsResponse.data[gameAppID].data
-      ) {
-        await ctx.reply('Failed to fetch additional game details from Steam.')
-        return
-      }
+    const gameDetailsUrl = `https://store.steampowered.com/api/appdetails/?appids=${gameAppID}`
+    const gameDetailsResponse = await axios.get(gameDetailsUrl)
 
-      const gameDetails = gameDetailsResponse.data[gameAppID].data
-      const gameDescription =
-        gameDetails.short_description || 'No description available.'
-      const gameImage = gameDetails.header_image || null
-      const gamePrice = gameDetails.price_overview
-        ? `${gameDetails.price_overview.final_formatted} (${gameDetails.price_overview.discount_percent}% off)`
-        : 'Price not available'
-      const gameReleaseDate = gameDetails.release_date
-        ? new Date(gameDetails.release_date.date).toDateString()
-        : 'Release date not available'
+    if (
+      !gameDetailsResponse ||
+      !gameDetailsResponse.data ||
+      !gameDetailsResponse.data[gameAppID] ||
+      !gameDetailsResponse.data[gameAppID].data
+    ) {
+      await ctx.reply('Failed to fetch additional game details from Steam.')
+      return
+    }
 
-      const inlineKeyboard = new InlineKeyboard()
-      inlineKeyboard.url(
-        'View on Steam',
-        `https://store.steampowered.com/app/${gameAppID}`
-      )
+    const gameDetails = gameDetailsResponse.data[gameAppID].data
+    const gameDescription =
+      gameDetails.short_description || 'No description available.'
+    const gameImage = gameDetails.header_image || null
+    const gamePrice = gameDetails.price_overview
+      ? `${gameDetails.price_overview.final_formatted} (${gameDetails.price_overview.discount_percent}% off)`
+      : 'Price not available'
+    const gameReleaseDate = gameDetails.release_date
+      ? new Date(gameDetails.release_date.date).toDateString()
+      : 'Release date not available'
 
-      if (gameImage !== null) {
-        await ctx.replyWithPhoto(gameImage, {
-          caption: `<b>${gameName}</b>\n${gameDescription}\n\n<b>Price:</b> ${gamePrice}\n<b>Release Date:</b> ${gameReleaseDate}`,
-          parse_mode: 'HTML',
+    const inlineKeyboard = new InlineKeyboard()
+    inlineKeyboard.url(
+      'View on Steam',
+      `https://store.steampowered.com/app/${gameAppID}`
+    )
+
+    if (gameImage !== null) {
+      await ctx.replyWithPhoto(gameImage, {
+        caption: `<b>${gameName}</b>\n${gameDescription}\n\n<b>Price:</b> ${gamePrice}\n<b>Release Date:</b> ${gameReleaseDate}`,
+        parse_mode: 'HTML',
+        reply_markup: inlineKeyboard,
+      })
+    } else {
+      await ctx.reply(
+        `<b>${gameName}</b>\n${gameDescription}\n\n<b>Price:</b> ${gamePrice}\n<b>Release Date:</b> ${gameReleaseDate}`,
+        {
           reply_markup: inlineKeyboard,
-        })
-      } else {
-        await ctx.reply(
-          `<b>${gameName}</b>\n${gameDescription}\n\n<b>Price:</b> ${gamePrice}\n<b>Release Date:</b> ${gameReleaseDate}`,
-          {
-            reply_markup: inlineKeyboard,
-            parse_mode: 'HTML',
-          }
-        )
-      }
-    } catch (error) {
-      console.error('Error fetching game data from Steam:', error.message)
-      await ctx.reply('An error occurred while fetching game data from Steam.')
+          parse_mode: 'HTML',
+        }
+      )
     }
   },
 }
