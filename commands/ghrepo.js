91e7abee3ac9790dbce475a2856a32c025d74e3a
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-09-27 22:24:28 +0530

    CannyBot: ghrepo: remove try/catch and check if no repo is found

--- commands/ghrepo.js
+++ commands/ghrepo.js
@@ -18,28 +18,24 @@ module.exports = {
 
     const repo = text.substring(text.indexOf(' ') + 1)
 
-    try {
-      const response = await axios(
-        `https://api.github.com/search/repositories?q=${repo}`
-      )
+    const response = await axios(
+      `https://api.github.com/search/repositories?q=${repo}`
+    )
 
-      const data = response.data.items[0]
-
-      await ctx.reply(
-        `<b>Repository:</b> <code>${data.name}</code>\n<b>Description:</b> ${data.description}\n<b>Language:</b> ${data.language}\n<b>Stars:</b> ${data.stargazers_count}\n<b>Forks:</b> ${data.forks_count}\n<b>Open Issues:</b> ${data.open_issues_count}\n<b>License:</b> ${data.license.name}\n<b>Owner:</b> ${data.owner.login}`,
-        {
-          reply_markup: new InlineKeyboard().url(
-            'View on GitHub',
-            data.html_url
-          ),
-          disable_web_page_preview: true,
-          parse_mode: 'HTML',
-        }
-      )
-    } catch (error) {
-      await ctx.reply(
-        'Oops, an error occurred!\n\nPossible reasons:\n• User not found\n• Rate limit exceeded'
-      )
+    if (response.data.total_count === 0) {
+      await ctx.reply('No repository found')
+      return
     }
+
+    const data = response.data.items[0]
+
+    await ctx.reply(
+      `<b>Repository:</b> <code>${data.name}</code>\n<b>Description:</b> ${data.description}\n<b>Language:</b> ${data.language}\n<b>Stars:</b> ${data.stargazers_count}\n<b>Forks:</b> ${data.forks_count}\n<b>Open Issues:</b> ${data.open_issues_count}\n<b>License:</b> ${data.license.name}\n<b>Owner:</b> ${data.owner.login}`,
+      {
+        reply_markup: new InlineKeyboard().url('View on GitHub', data.html_url),
+        disable_web_page_preview: true,
+        parse_mode: 'HTML',
+      }
+    )
   },
 }
