d8a4c07a7bc4acb8434f1633923a929b37d8b143
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-09-27 23:39:27 +0530

    CannyBot: weather: remove try-catch block

--- commands/weather.js
+++ commands/weather.js
@@ -19,40 +19,42 @@ module.exports = {
     const location = text.substring(text.indexOf(' ') + 1)
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHERMAP_API_KEY}&units=metric`
 
-    try {
-      const { data } = await axios.get(url)
+    const response = await axios.get(url).catch((err) => {
+      if (err.response.status === 404) {
+        ctx.reply('Location not found')
+        return
+      }
+    })
 
-      const { name, sys, weather, main, wind } = data
-      const { country } = sys
-      const { temp, feels_like, temp_min, temp_max, humidity } = main
-      const { speed } = wind
-      const { main: weatherMain, description } = weather[0]
-
-      const keyboard = new InlineKeyboard().url(
-        'OpenWeatherMap',
-        `https://openweathermap.org/city/${data.id}`
-      )
-
-      await ctx.reply(
-        `Weather in ${name}, ${country}\n\n` +
-          `🌡️ Temperature: ${temp}°C\n` +
-          `🌡️ Feels like: ${feels_like}°C\n` +
-          `🌡️ Min: ${temp_min}°C\n` +
-          `🌡️ Max: ${temp_max}°C\n` +
-          `💧 Humidity: ${humidity}%\n` +
-          `💨 Wind speed: ${speed} m/s\n` +
-          `🌤️ Weather: ${weatherMain} (${description})`,
-        {
-          reply_markup: keyboard,
-        }
-      )
-    } catch (error) {
-      ctx.reply(
-        'Oops, something went wrong\n\n' +
-          'Possible reasons:\n' +
-          '1. Invalid location\n' +
-          '2. API error'
-      )
+    if (!response) {
+      return
     }
+
+    const data = response.data
+
+    const { name, sys, weather, main, wind } = data
+    const { country } = sys
+    const { temp, feels_like, temp_min, temp_max, humidity } = main
+    const { speed } = wind
+    const { main: weatherMain, description } = weather[0]
+
+    const keyboard = new InlineKeyboard().url(
+      'OpenWeatherMap',
+      `https://openweathermap.org/city/${data.id}`
+    )
+
+    await ctx.reply(
+      `Weather in ${name}, ${country}\n\n` +
+        `🌡️ Temperature: ${temp}°C\n` +
+        `🌡️ Feels like: ${feels_like}°C\n` +
+        `🌡️ Min: ${temp_min}°C\n` +
+        `🌡️ Max: ${temp_max}°C\n` +
+        `💧 Humidity: ${humidity}%\n` +
+        `💨 Wind speed: ${speed} m/s\n` +
+        `🌤️ Weather: ${weatherMain} (${description})`,
+      {
+        reply_markup: keyboard,
+      }
+    )
   },
 }
